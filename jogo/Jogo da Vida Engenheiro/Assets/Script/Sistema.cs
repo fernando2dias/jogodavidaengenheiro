4772caf0a52f2200e2ea0275417cfa1638a2f9fc
Unknown <michel_michelot@hotmail.com>
2018-10-06 20:48:30 -0300

    Movimentação pronta

---
+++ jogo/Jogo da Vida Engenheiro/Assets/Script/Sistema.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Sistema : MonoBehaviour {
+
+    public Transform[] Casas;
+    int Casaatual = 0;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Casaatual = Casaatual + DiceNumberTextScript.diceNumber;
+            if (Casaatual < Casas.Length)
+            {
+                transform.position = Casas[Casaatual].position;
+                Debug.Log(Casaatual);
+            }
+            else
+            {
+                Casaatual = 46;
+                transform.position = Casas[Casaatual].position;
+            }
+        }
+    }
+
+}
